fe9e8180e539f25fdbb525583e599e8196c50f91
Marvin <marv.1997@live.de>
2022-01-03 22:01:00 +0100

    Implement the close syscall naively

--- jvm/syscalls/syscall1.py
+++ jvm/syscalls/syscall1.py
@@ -9,15 +9,38 @@ def syscall1_method_instructions(context: GenerateContext):
         .load_long(2)
         .convert_long_to_integer()
         .lookup_switch("exit0", {
+            SysCalls.CLOSE: "close",
             SysCalls.EXIT: "exit"
         })
+
+        .label("close")
+        .new(context.cf.constants.create_class("java/io/FileInputStream"))
+        .duplicate_top_of_stack()
+        .get_static_field(context.fd_ref)
+        .load_long(0)  # file descriptor
+        .convert_long_to_integer()
+        .load_array_reference()
+        .invoke_special(context.cf.constants.create_method_ref("java/io/FileInputStream", "<init>",
+                                                               "(Ljava/io/FileDescriptor;)V"))
+        .invoke_virtual(context.cf.constants.create_method_ref("java/io/FileInputStream", "close", "()V"))
+        .get_static_field(context.fd_ref)
+        .load_long(0)  # file descriptor
+        .convert_long_to_integer()
+        .push_null()
+        .store_array_reference()
+
+        .branch("exit0")
+        .end_branch()
+
         .label("exit")
         .load_long(0)
         .convert_long_to_integer()
         .invoke_static(context.cf.constants.create_method_ref("java/lang/System", "exit", "(I)V"))
-        .push_long(0)
-        .return_long()
+        .branch("exit0")
+        .end_branch()
+
         .label("exit0")
+        .end_branch()
         .push_long(0)
         .return_long()
     )
